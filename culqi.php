2a2a7797c2d9e0ca8b34935bc9835509765a0f14
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-09-11 15:29:18 -0500

    ajustes

--- culqi.php
+++ culqi.php
@@ -46,7 +46,7 @@ class Culqi extends PaymentModule
     {
         $this->name = 'culqi';
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.4';
+        $this->version = '3.0.0';
         $this->controllers = array('chargeajax', 'postpayment', 'generateorder', 'merchantajax', 'webhook', 'registersale');
         $this->author = 'Culqi';
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
