24c269ca83d7c8f45301441bb5e1118d0bef4903
JordanDiaz2022 <jordan.diaz@culqi.com>
2022-11-04 17:54:14 -0500

    hotfix charges and orders

--- controllers/front/registersale.php
+++ controllers/front/registersale.php
@@ -16,29 +16,31 @@ class CulqiRegisterSaleModuleFrontController extends ModuleFrontController
         $this->module->validateOrder((int)$cart->id, Configuration::get('CULQI_STATE_PENDING'), (float)$cart->getordertotal(true), 'Culqi', null, array(), (int)$cart->id_currency, false, $customer->secure_key);
 
         $id_order = Order::getOrderByCartId($this->context->cart->id);
-        $order = new Order($id_order);
-        $order_payment_collection = $order->getOrderPaymentCollection();
+        if($id_order!=null)
+        {
+            $order = new Order($id_order);
+            $order_payment_collection = $order->getOrderPaymentCollection();
 
-        $order_payment = $order_payment_collection[0];
-        $order_payment->transaction_id = Tools::getValue("order_id");
-        $order_payment->update();
-        //
-        $culqiPretashop =  new Culqi();
-        $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
-        $enviroment_cart = $infoCheckout['enviroment_backend'];
-        $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
-        $args_order = array(
-            'enviroment' => $enviroment_cart,
-            'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
-         );
-        try{
-            $culqi_order = $culqi->Orders->update( Tools::getValue("order_id"), $args_order );
-        }catch (Exception $e){
-            echo '<script type="text/javascript">console.log("Error en el update de cargo!"); </script>';
+            $order_payment = $order_payment_collection[0];
+            $order_payment->transaction_id = Tools::getValue("order_id");
+            $order_payment->update();
+            //
+            $culqiPretashop =  new Culqi();
+            $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
+            $enviroment_cart = $infoCheckout['enviroment_backend'];
+            $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
+            $args_order = array(
+                'enviroment' => $enviroment_cart,
+                'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
+            );
+            try{
+                $culqi_order = $culqi->Orders->update( Tools::getValue("order_id"), $args_order );
+            }catch (Exception $e){
+                echo '<script type="text/javascript">console.log("Error en el update de cargo!"); </script>';
+            }
+
+            die(Tools::jsonEncode($id_order));
         }
-
-        die(Tools::jsonEncode($id_order));
-
     }
 
 }
