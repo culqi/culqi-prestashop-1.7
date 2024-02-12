3729e7614f700b4f00d9dd7925193366de55776d
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2024-02-12 16:41:39 -0500

    v3.0.8

--- culqi.php
+++ culqi.php
@@ -5,7 +5,7 @@ use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
 if (!defined('_PS_VERSION_'))
     exit;
 
-define('CULQI_PLUGIN_VERSION', '3.0.7');
+define('CULQI_PLUGIN_VERSION', '3.0.8');
 
 define('URLAPI_INTEG', 'https://integ-panel.culqi.com');
 define('URLAPI_PROD', 'https://panel.culqi.com');
