887f293ae15c067bd96705102c707c8a2c92f63a
Tekton <lorenaortizdezevallos@MacBook-Pro-de-Lorena.local>
2024-05-03 12:26:29 -0500

    fix: php8.2 support

--- culqi.php
+++ culqi.php
@@ -38,7 +38,7 @@ include_once dirname(__FILE__) . '/libraries/Requests/library/Requests.php';
 Requests::register_autoloader();
 
 //include_once dirname(__FILE__).'/libraries/culqi-php/lib/culqi.php';
-
+#[AllowDynamicProperties]
 class Culqi extends PaymentModule
 {
 
